70fd5ca2fb24432155062e9372699aac60b4c236
LoxductheGreat <jameelsalhhadin@gmail.com>
2020-08-18 14:47:17 -0400

    got stuck on finding averages

--- homework.js
+++ homework.js
@@ -5,15 +5,45 @@
 //
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+function remove (s, r){
+let studentsremoved =[];
+for (let word of s){
+    if (word !== r) {
+        studentsremoved.push(word)
+        }
+    }
+    return studentsremoved
+}
+
 
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(nums){
+    let sum = 0;
+        for (let n of nums){
+            sum += n
+        }
+    return  sum
+}
+
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+
+function average(nums){
+    let sum = 0;
+    let count = 0;
+    
+    
+       
+        
+        let average = sum / count;
+    return average
+}
+
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
